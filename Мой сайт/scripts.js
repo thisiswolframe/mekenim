c34018eed641c0e4f49bc6a38c0b2d9d44bb6f21
thisiswolframe <148793034+thisiswolframe@users.noreply.github.com>
2025-02-09 21:30:33 +0600

    Add files via upload

--- Мой сайт/scripts.js
+++ Мой сайт/scripts.js
@@ -1,3 +1,4 @@
+// Скрипт для открытия и закрытия мобильного меню-гамбургера
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.getElementById("menuToggle");
   const menuClose = document.getElementById("menuClose");
@@ -5,11 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   menuToggle.addEventListener("click", function (event) {
       event.preventDefault();
-      mobileMenu.classList.add("active"); // Открываем меню
+      mobileMenu.classList.add("active"); 
   });
 
   menuClose.addEventListener("click", function (event) {
       event.preventDefault();
-      mobileMenu.classList.remove("active"); // Закрываем меню
+      mobileMenu.classList.remove("active"); 
   });
+});
+
+// Скрипт открывания заднего фона для навигационного меню при скроллинге
+window.addEventListener("scroll", function() {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+  } else {
+      navbar.classList.remove("scrolled");
+  }
 });
